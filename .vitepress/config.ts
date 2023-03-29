37057bb17545fede158dfbe6d3c7ee6a4322cffb
Damian Pietrzyk <dpietrzyk96@gmail.com>
2023-03-29 20:36:30 +0200

    add deploy

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "dpietrzyk",
   description: "dpietrzyk - personal space",
+  base: '/dpietrzyk/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
